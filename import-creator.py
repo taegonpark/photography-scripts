773875af0391732d78dc3b0de5f68f4f2d36cc51
Taegon <bizaroad@gmail.com>
2025-05-10 20:31:50 -0400

    Add subfolder names as input

--- import-creator.py
+++ import-creator.py
@@ -2,16 +2,16 @@ import os
 
 extensions = ('.avif', '.jpg', '.jpeg', '.png', '.webp')
 
-def generateImportStatements (folderPath, fObj, startIndex, arrayName):
+def generateImportStatements (folderPath, fObj, startIndex, folderName, arrayName):
     images = os.listdir(folderPath)
     num = startIndex
     for i, imageName in enumerate(images):
         if imageName.lower().endswith(extensions):
             fObj.write("import img")
             fObj.write(str(num))
-            fObj.write(" from ")
+            fObj.write(f" from './assets/{folderName}/")
             fObj.write(imageName)
-            fObj.write(";\n")
+            fObj.write("';\n")
             num = num + 1
     fObj.write(f"const {arrayName} = [")
     for i in range (startIndex, num):
@@ -22,5 +22,6 @@ def generateImportStatements (folderPath, fObj, startIndex, arrayName):
     return num
 
 with open("import-statements.js", "w") as f:
-    nextStart = generateImportStatements('./assets/folder1', f, 0, "folder1")
-    nextStart = generateImportStatements('./assets/folder2', f, nextStart, "folder2")
+    nextStart = generateImportStatements('../photography-webpage/src/assets/concerts', f, 0, "concerts-reduced", "concertImages")
+    nextStart = generateImportStatements('../photography-webpage/src/assets/trips', f, nextStart, "trips-reduced", "tripImages")
+    nextStart = generateImportStatements('../photography-webpage/src/assets/other', f, nextStart, "other-reduced", "otherImages")
